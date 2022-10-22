5cfd7b4ee9b5667b81e35ec0598d83107e4efc21
Mourad RL CADI <mouradface1998@gmail.com>
2022-10-22 18:27:31 +0100

    test

--- ts/course-ts/type-assertion/index.ts
+++ ts/course-ts/type-assertion/index.ts
@@ -1,26 +1,18 @@
 /**
  * Checks if T1 equals to T2.
  */
- export type IsTypeEqual<T1, T2> = IsNotAny<T1> extends false ? false : (
-    IsNotAny<T2> extends false ? false : (
-        [T1] extends [T2] ? ([T2] extends [T1] ? true : false): false
-    )
-);
+export type IsTypeEqual<T1, T2> = IsNotAny<T1> extends false ? false : IsNotAny<T2> extends false ? false : [T1] extends [T2] ? ([T2] extends [T1] ? true : false) : false;
 
 /**
  * Checks if T2 can be assigned to T1.
  */
-export type IsTypeAssignable<T1, T2> = IsNotAny<T1> extends false ? false : (
-    IsNotAny<T2> extends false ? false : (
-        [T2] extends [T1] ? true : false
-    )
-);
+export type IsTypeAssignable<T1, T2> = IsNotAny<T1> extends false ? false : IsNotAny<T2> extends false ? false : [T2] extends [T1] ? true : false;
 
 /**
  * Returns `false` if `any` is specified, otherwise returns `true`.
  * @see https://stackoverflow.com/a/49928360/3406963
  */
-export type IsNotAny<T> = 0 extends (1 & T) ? false : true;
+export type IsNotAny<T> = 0 extends 1 & T ? false : true;
 
 /**
  * Returns true for false and vice versa.
